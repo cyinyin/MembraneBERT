45a28f0106cf25313c30832be7225fa34f334cfe
cyinyin <94597141+cyinyin@users.noreply.github.com>
2025-11-02 18:15:49 +0800

    Implement core functionality for paper connection scraping
    Added functional objectives and main execution logic for scraping and writing results.

--- ConectedPapers/main.py
+++ ConectedPapers/main.py
@@ -1,1 +1,26 @@
+# coding=utf-8
+"""
+# Functional Objectives
+- Identify core papers: listed in a TXT file, one title per line.
+- Search for the paper titles on [https://www.connectedpapers.com/](https://www.connectedpapers.com/) and expand the relationship graph based on the first result.
+- Collect relevant information (paper details and connections) and store it in a database.
+- Construct a relationship graph based on the connections using BFS (Breadth-First Search).
+- Generate other file formats.
+"""
+from unit import Args
+from unit.connection import spider
+from unit.write import Write
+if __name__ == "__main__":
+    _args = Args()
+    # _args.log.init()
+
+    # Scrape related literature
+    spider(_args)
+
+    # Write results to a file
+    w = Write(_args)
+    w.to_excel()
+    w.to_markdown()
+
+    pass
 
